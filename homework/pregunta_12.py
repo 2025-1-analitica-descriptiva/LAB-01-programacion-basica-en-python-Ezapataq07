93add69e66dd707e7846995abad6be1166d34277
Ezapataq07 <ezapataq@unal.edu.co>
2025-05-11 14:40:07 -0500

    solution 12

--- homework/pregunta_12.py
+++ homework/pregunta_12.py
@@ -6,6 +6,9 @@ utilizar pandas, numpy o scipy.
 """
 
 
+from itertools import groupby
+
+
 def pregunta_12():
     """
     Genere un diccionario que contengan como clave la columna 1 y como valor
@@ -15,3 +18,16 @@ def pregunta_12():
     {'A': 177, 'B': 187, 'C': 114, 'D': 136, 'E': 324}
 
     """
+    with open("files/input/data.csv", 'r') as file:
+        lines = file.readlines()
+        sequence = []
+        for line in lines:
+            columns = line.strip().split('\t')
+            key_values = columns[4].split(',')
+            sequence.extend((columns[0], int(kv.split(':')[1])) for kv in key_values)
+    
+    result = {}
+    for key, group in groupby(sorted(sequence, key=lambda x: x[0]), key=lambda x: x[0]):
+        count = sum(value for _, value in group)
+        result[key] = count
+    return result
